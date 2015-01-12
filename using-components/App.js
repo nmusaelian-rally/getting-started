34a894090b88b677a4a53d2bb928d5b4bb999128
nmusaelian <nmusaelian@rallydev.com>
2015-01-12 13:49:54 -0700

    first commit using-comp

--- using-components/App.js
+++ using-components/App.js
@@ -13,6 +13,19 @@ Ext.define('Rally.gettingstarted.UsingComponents', {
         //defect priorities.  Wire up event listeners for
         //when the combobox is initially loaded and when
         //its value changes
+        this.add({
+            xtype: 'rallyfieldvaluecombobox',
+            itemId: 'priorityCombobox',
+            fieldLabel: 'Filter by Priority:',
+            model: 'Defect',
+            field: 'Priority',
+            context: this.getContext().getDataContext(),  //returns obj containing workspace, proj, projectScopeUp and projectScopeDown properties
+            listeners: {
+                ready: this._onLoad,
+                select:this._onSelect,
+                scope: this
+            }
+        });
     },
 
     /**
@@ -20,11 +33,36 @@ Ext.define('Rally.gettingstarted.UsingComponents', {
      * add a grid of defects for the selected priority
      */
     _onLoad: function() {
+        this.add({
+            xtype: 'rallygrid',
+            columnCfgs: [
+                'Name',
+                'FormattedID',
+                'Priority',
+                'Severity'
+            ],
+            context: this.getContext(),
+            storeConfig:{
+                model: 'Defect',
+                filters:[this._getPriorityFilter()]
+            }
+        });
     },
 
     /**
      * Refresh the grid when the combobox changes value
      */
     _onSelect: function() {
+        var grid = this.down('rallygrid') ;
+        var store = grid.getStore();
+        store.clearFilter(true);
+        store.filter(this._getPriorityFilter());
+    } ,
+    _getPriorityFilter: function() {
+        return {
+            property: 'Priority',
+            operator: '=',
+            value:  this.down('#priorityCombobox').getValue()
+        };
     }
 });
